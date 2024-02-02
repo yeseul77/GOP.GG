d2b168b3871b1097cf8491f7f2490b431e6377c8
jihwan <jh03112888@gmail.com>
2024-02-02 14:37:34 +0900

    임시 커밋

--- src/main/java/com/gg/gop/controller/MainController.java
+++ src/main/java/com/gg/gop/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.gg.gop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/")
-	public String Main() {
-		return "index";
-	}
+//	@GetMapping("/")
+//	public String Main() {
+//		return "index";
+//	}
 
 }
