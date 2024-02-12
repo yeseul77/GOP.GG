9617e0d4fad146710e8434fdda11be95673b574e
takealook <82574092+wjstmddk@users.noreply.github.com>
2024-02-13 08:45:39 +0900

    갈아엎기 완료

--- src/main/java/com/gg/gop/service/MemberService.java
+++ src/main/java/com/gg/gop/service/MemberService.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.gg.gop.dao.MemberDao;
 import com.gg.gop.dto.MemberDto;
 
-import oracle.jdbc.proxy.annotation.Post;
+//import oracle.jdbc.proxy.annotation.Post;
 
 
 @Service
