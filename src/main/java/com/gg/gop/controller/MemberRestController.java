4650059d4c15d21ba8f31478a2cfb7c856c43d37
takealook <82574092+wjstmddk@users.noreply.github.com>
2024-02-26 08:38:45 +0900

    예슬누나 합치기-1

--- src/main/java/com/gg/gop/controller/MemberRestController.java
+++ src/main/java/com/gg/gop/controller/MemberRestController.java
@@ -1,25 +1,30 @@
 package com.gg.gop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.gg.gop.service.MemberService;
-import lombok.extern.slf4j.Slf4j;
 
-@Controller
+@RestController
 public class MemberRestController {
-	@Autowired
-	private MemberService memberService;
-	
-	//아이디 중복체크 
-    @GetMapping("/checkId")
-    public String CheckId(String m_id) {
-		String res=memberService.checkid(m_id);
-		return res;
-    }
-	
-    
-    
-    
 
+    @Autowired
+    private MemberService memberService;
+
+    @PostMapping("/confirmusername")
+    public ResponseEntity<Boolean> confirmUsername(@RequestParam("username") String username) {
+        boolean result;
+
+        if (username.trim().isEmpty()) {
+            result = false;
+        } else {
+            result = !memberService.selectusername(username);
+        }
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
--- src/main/java/com/gg/gop/dto/MemberDto.java
+++ src/main/java/com/gg/gop/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package com.gg.gop.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,7 +9,7 @@ import lombok.experimental.Accessors;
 
 @Accessors(chain = true)
 @Data
-@Builder // 빌더후 @AllArgsConstructor을 이용해 객체 생성함.
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
@@ -19,7 +20,60 @@ public class MemberDto {
 	private String password;
 	private String role; 
 	private Boolean deleteYn; // 탈퇴 여부 0이면가입 탈퇴면1
-	// private String m_tier; // 소환사 티어
-	// private LocalDateTime modifiedDate; // 최종 수정일시
+	private String profile;//사진정보
+	
+
+	
+	  public String getEmail() {
+	        return email;
+	    }
+
+	    public void setEmail(String email) {
+	        this.email = email;
+	    }
+
+
+	    public String getUsername() {
+	        return username;
+	    }
+
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+
+
+	    public String getPassword() {
+	        return password;
+	    }
+
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+
+
+	    public String getRole() {
+	        return role;
+	    }
+
+	    public void setRole(String role) {
+	        this.role = role;
+	    }
+
+	    public Boolean getDeleteYn() {
+	        return deleteYn;
+	    }
+
+	    public void setDeleteYn(Boolean deleteYn) {
+	        this.deleteYn = deleteYn;
+	    }
+
+		public String getProfile() {
+			return profile;
+		}
+
+		public void setProfile(String profile) {
+			this.profile = profile;
+		}
+
 	
 }
