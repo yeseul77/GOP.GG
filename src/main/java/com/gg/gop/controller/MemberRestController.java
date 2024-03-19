fab4a87f4f6a59f404a1e35cb3b6440cb835234a
YESEUL <134572370+yeseul77@users.noreply.github.com>
2024-03-19 14:02:40 +0900

    Update MemberRestController.java

--- src/main/java/com/gg/gop/controller/MemberRestController.java
+++ src/main/java/com/gg/gop/controller/MemberRestController.java
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-package com.gg.gop.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.gg.gop.service.MemberService;
-import lombok.extern.slf4j.Slf4j;
-import java.util.HashMap;
-import java.util.Map;
-=======
 package com.gg.gop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
->>>>>>> 460304782af17c227702360b2e841954ad0105a1
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gg.gop.service.MemberService;
 
 @RestController
 public class MemberRestController {
@@ -26,21 +15,19 @@ public class MemberRestController {
     @Autowired
     private MemberService memberService;
 
-<<<<<<< HEAD
-}
+    
 
-=======
     @PostMapping("/confirmusername")
     public ResponseEntity<Boolean> confirmUsername(@RequestParam("username") String username) {
-        boolean result;
-
-        if (username.trim().isEmpty()) {
-            result = false;
-        } else {
-            result = !memberService.selectusername(username);
+        Object result=memberService.selectusername(username);
+        if(!(result==null)){
+            return new ResponseEntity<>(false, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(true, HttpStatus.OK);
         }
 
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    
+    
     }
+    
 }
->>>>>>> 460304782af17c227702360b2e841954ad0105a1
