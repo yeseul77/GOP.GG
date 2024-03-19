072983539c153ef76c0872eb21d3c93cdc119af0
justlikethatHwan <154960945+justlikethatHwan@users.noreply.github.com>
2024-03-19 13:46:19 +0900

    Update SummonerContoller.java

--- src/main/java/com/gg/gop/controller/SummonerContoller.java
+++ src/main/java/com/gg/gop/controller/SummonerContoller.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.gg.gop.dto.SummonerDto;
 import com.gg.gop.service.SummonerService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class SummonerContoller {
 	@Autowired
@@ -25,8 +27,8 @@ public class SummonerContoller {
 
 	@GetMapping("/championSearchForm")
 	public String championSearchForm(Model model) {
-	//	List<ChampionRanking> rankingList = summonerService.getChampionRanking();
-	 //   model.addAttribute("rankingList", rankingList);
+//		List<ChampionRanking> rankingList = summonerService.getChampionRanking();
+//	    model.addAttribute("rankingList", rankingList);
 		return "championSearchForm"; // 챔피언 정보 페이지로 이동하는 뷰 이름을 반환합니다.
 	}
 
@@ -43,10 +45,9 @@ public class SummonerContoller {
 	    // 최근 10게임만 추출하고, 검색한 소환사 이름과 태그 라인이 일치하는 게임 정보만 필터링
 	    int gameCount = 0;
 	    for (Map<String, Object> gameData : combinedGameData) {
-	        if (gameCount >= 10) {
+	        if (gameCount >= 100) {
 	            break; // 최대 10게임까지만 처리
 	        }
-
 	        // 게임 데이터에서 플레이어 정보 추출
 	        List<Map<String, Object>> playerInfoList = (List<Map<String, Object>>) gameData.get("info");
 	        for (Map<String, Object> playerInfo : playerInfoList) {
